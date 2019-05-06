cd8807db71e2182efe8e55b24b1d0bf0c7f4c024
隋霖 <401569798@qq.com>
2019-05-06 11:25:15 +0800

    add the handcrafted version KNN

--- KNN.py
+++ KNN.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-from .dataRead import wordList, classDict, wordD
 from sklearn.feature_extraction.text import CountVectorizer
 from SklearnVersionDataset import SklearnVersionDataset
 
@@ -9,5 +8,30 @@ class KNN(object):
         # 特征就不手动提取了
         S = SklearnVersionDataset()
         stringDataset, cls = S.get()
+        print("文件读取完毕")
         vectorizer = CountVectorizer()
-        X = vectorizer.fit_transform()
+        X = vectorizer.fit_transform(stringDataset)
+        print("特征读取完成")
+        self.feature = X.toarray()
+        self.K = K
+        self.clsSet = list(set(cls))
+        self.clsDict = {}
+        for i in self.clsSet:
+            self.clsDict[i] = i
+    def cal(self):
+        print("开始计算类别")
+        for i in self.feature:
+            print(i)
+            current = np.sum(np.multiply(i*self.feature), 1)
+            print("1")
+            k = sorted(enumerate(current), reverse=True, key=lambda x:x[1])
+            print("2")
+            k = k[:self.K]
+            d = np.zeros((1, len(self.clsSet)))
+            for i in k:
+                d[self.clsDict[self.cls[i]]] += 1
+            index = int(np.where(d==np.max(d)))
+            print(self.clsSet[index])
+
+K = KNN()
+K.cal()
--- dataProcess_KNN.py
+++ dataProcess_KNN.py
@@ -1,40 +1,29 @@
 import os
 import jieba
-import pandas
+import numpy as np
 
-# 给KNN提供数据的数据处理 单独记录所有文件的词频和分类
+# 自己提取特征的效率很慢...
 
-def wordRecord(wordsDict):
-    dataFrame = pandas.DataFrame(wordsDict)
-    filePath = "./KNNDataSet.csv"
-    dataFrame.to_csv(filePath,sep=",",header=True,index=True,encoding="utf_8_sig")
+# KNN因为文件数目太多了... 提取了20000个单词作为计算 10000 * 20000
+wordListFile = "./words.txt"
+wordFile = open(wordListFile, "r", encoding="utf-8")
+wordList = wordFile.readline().split(",")
+wordDict = {}
+# 建立字符和数组位置的映射
+for i in range(len(wordList)):
+    wordDict[wordList[i]] = i
 
+# 开始统计单词频率矩阵
+wordMatrix = np.zeros((9833, 20000)) # 9833个文件 20000个单词
+# 记录文件名 和类别
+fileMessage = []
+# 记录的时候 每行 -> 文件名 类别 20000个单词出现的频率
 datasetDir = "./dataset"
 classL = os.listdir(datasetDir)
-classDict = {}
-for i in classL:
-    classDict[i] = 0
+
 classList = [os.path.join(datasetDir, i) for i in classL if os.path.isdir(os.path.join(datasetDir, i)) and i.find("C")>=0]
-#PS: 1.编码格式是gbk等
-#    2.三种可能的空格\u00A0,\u0020,\u3000 先替换成 空格然后在分割
-print(classList)
-decode = "GB2312"
-# 过滤掉的标点符号等
-wordFilter = ["，","。","、","？","；","：","“","”","‘","’","（","）","【","】","《","》","！", " ","-","~"]
-wordsDict = {
-
-}
 index = 0
-fileDict = {
-
-}
-for i in range(len(classList)):
-    currentClass = classL[i]
-    fileList = os.listdir(classList[i])
-    fileList = [os.path.join(classList[i], j) for j in fileList if j.find(".txt") >= 0]
-    for F in fileList:
-        fileDict[F+"__"+currentClass] = 0
-
+fi = 0
 for i in classList:
     print(index)
     currentClass = classL[index]
@@ -42,6 +31,7 @@ for i in classList:
     fileList = [os.path.join(i, j) for j in fileList if j.find(".txt")>=0]
     for F in fileList:
         print(F)
+        fileMessage.append((F, currentClass))
         file = open(F, "r", encoding="GBK", errors="ignore").read()
         # 去掉所有的\n
         file = file.replace("\n", " ")
@@ -50,14 +40,19 @@ for i in classList:
         # 进行分词
         words = jieba.cut(file)
         for word in words:
-            if word not in wordFilter:
-                if word not in wordsDict:
-                    wordsDict[word] = fileDict.copy()
-                    wordsDict[word][F+"__"+currentClass] = 1
-                else:
-                    wordsDict[word][F+"__"+currentClass] += 1
+            if word in wordList:
+               wordMatrix[fi][wordDict[word]] += 1
+        fi += 1
+        if fi > 5:
+            break
+    break
     index += 1
-
-wordRecord(wordsDict)
+# 开始进行矩阵的记录:
+recordFile = open("KNNDataset.txt","w")
+wordMatrix = wordMatrix.astype("<U6")
+for i in range(len(fileMessage)):
+    print(i)
+    recordFile.write("{0},{1},{2}\n".format(fileMessage[i][0],fileMessage[i][1],",".join(wordMatrix[i])))
+recordFile.close()
 # print(p)
 
